c422e6050b33951066a9a8ec77e1fe4665df4ebb
abi-cloud <2811abarna@gmail.com>
2024-09-15 17:09:31 +0100

    /

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
-// console.log ("Hello")
 document.addEventListener("DOMContentLoaded", () => {
 
     const images = document.querySelectorAll("img");
-    //this grabs all the images
 
     for(const image of images){
-        //console.log(image)
+    
         fetch("https://dog.ceo/api/breeds/image/random")
         .then(response => response.json ())
         .then(data => {
@@ -13,7 +11,6 @@ document.addEventListener("DOMContentLoaded", () => {
             image.width = 100;
             image.height = 100;
         })
-        //each src to be replaced with that of the data message, which is a random dog picture, the message itself will be the URL
+       
     }
 })
-//Each image's src should be something new every single time.
